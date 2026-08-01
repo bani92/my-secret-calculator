60243604663c2ec0bf504d3b2a1e4892356623ef
bani <dalllo9851@gmail.com>
2026-08-01 14:52:20 +0900

    test: stabilize app tests against current date

--- src/App.test.ts
+++ src/App.test.ts
@@ -121,6 +121,8 @@ async function flushAsyncActions(): Promise<void> {
 
 describe('App', () => {
   beforeEach(() => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date('2026-07-18T09:00:00.000Z'));
     setActivePinia(createPinia());
     budgetData = createEmptyBudgetData();
     budgetWritesShouldFail = false;
